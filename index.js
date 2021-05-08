9b2396cc2896b4213929c2656636d0f0f57ce06c
Darkzarich <darkzarichv2@gmail.com>
2021-05-08 18:25:51 +0300

    make collide logic & power concept & unit respawn

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 const BORDER = 400;
 const SCORE_SEPARATOR_WIDTH = 4;
+const UNIT_SIZE = 14;
 const PLAYER1_COLOR = 'green';
 const PLAYER2_COLOR = 'maroon';
+const UNIT_POWER_GROWTH = 1;
 class Unit {
-  constructor({x, y, side}, ctx) {
-    this.x = x
-    this.y = y
-    this.side = side
-    this.$ctx = ctx
+  constructor({ x, y, side }, ctx) {
+    this.x = x;
+    this.y = y;
+    this.side = side;
+    this.collided = true;
+    (this.$ctx = ctx), (this.power = 0);
   }
 
   move() {
     this.renderUnit(this.x, this.y, this.side);
-  
+
+    this.handleOutOfBorder();
+
+    this.x = this.x + 1 * getDirectionBySide(this.side);
+  }
+
+  handleOutOfBorder() {
     if (this.x > BORDER) {
-      this.y = this.y + 16;
+      this.y = this.y + Math.floor(Math.random() * 100) % UNIT_SIZE; // random drop down
       this.x = 0;
     }
-  
+
     if (this.x < 0) {
-      this.y = this.y + 16;
+      this.y = this.y + Math.floor(Math.random() * 100) % UNIT_SIZE; // random drop down
       this.x = BORDER;
     }
-  
+
     if (this.y > BORDER) {
       this.y = 0;
     }
-  
-    this.x = this.x + 1 * getDirectionBySide(this.side);
+  }
+
+  setPower(power) {
+    this.power = power;
   }
 
   renderUnit() {
     this.$ctx.fillStyle = this.side;
-    this.$ctx.fillRect(this.x, this.y, 14, 14);
+    this.$ctx.fillRect(this.x, this.y, UNIT_SIZE, UNIT_SIZE);
   }
 }
 
 class UI {
   constructor(ctx) {
-    this.$ctx = ctx
+    this.$ctx = ctx;
   }
 
   clearFrame() {
@@ -50,7 +61,7 @@ class UI {
     // Draw separator
     this.$ctx.fillStyle = 'white';
     this.$ctx.fillRect(BORDER, 0, SCORE_SEPARATOR_WIDTH, BORDER);
-  
+
     // Draw player1 UI side
     this.$ctx.fillStyle = PLAYER1_COLOR;
     this.$ctx.fillRect(
@@ -59,7 +70,7 @@ class UI {
       BORDER - SCORE_SEPARATOR_WIDTH + 100,
       BORDER / 2
     );
-  
+
     // Draw player2 UI side
     this.$ctx.fillStyle = PLAYER2_COLOR;
     this.$ctx.fillRect(
@@ -71,38 +82,88 @@ class UI {
   }
 }
 
-class Game {
-  constructor(ctx, ui, unitsTotal, sides) {
-    this.$ctx = ctx
-    this.ui = ui;
-    this.units = this.generateRandomUnits(unitsTotal, sides)
+class Units {
+  constructor(ctx, unitsTotal, sides) {
+    this.$ctx = ctx;
+    this.units = this.generateRandomUnits(unitsTotal, sides);
   }
 
   generateRandomUnits(unitsTotal, sides) {
-    let i = 0
-  
+    let i = 0;
+
     return new Array(unitsTotal).fill().map(() => {
       if (i < sides.length - 1) i = i + 1;
-      else i = 0
+      else i = 0;
 
-      return new Unit({
+      return this.generateRandomUnit(sides[i])
+    });
+  }
+
+  generateRandomUnit(side) {
+    return new Unit(
+      {
         x: Math.floor(Math.random() * 1000) % 400,
         y: Math.floor(Math.random() * 1000) % 400,
-        side: sides[i],
-      }, this.$ctx);
+        side,
+      },
+      this.$ctx
+    );
+  }
+
+  moveUnits() {
+    this.units.forEach((unit) => {
+      unit.move();
+      const collidedUnit = this.getCollidedUnit(unit)
+
+      if (collidedUnit) {
+        if (unit.power >= collidedUnit.power) {
+          unit.setPower(0);
+          this.killUnit(collidedUnit)
+        } else {
+          collidedUnit.setPower(0);
+          this.killUnit(unit)
+        }  
+        
+        return;
+      }
+
+      unit.setPower(unit.power + UNIT_POWER_GROWTH)
     });
+  }
+
+  killUnit(unit) {
+    this.units = this.units.filter(u => u !== unit)
+    this.units.push(this.generateRandomUnit(unit.side))
+  }
+
+  getCollidedUnit(unit) {
+    for (const otherUnit of this.units) {
+      if (
+        Math.abs(otherUnit.x - unit.x) <= UNIT_SIZE - 2 &&
+        Math.abs(otherUnit.y - unit.y) <= UNIT_SIZE - 2 &&
+        otherUnit.side !== unit.side
+      ) {
+        return otherUnit
+      }
+    }
+  }
+}
+
+class Game {
+  constructor(ctx, ui, unitsTotal, sides) {
+    this.$ctx = ctx;
+    this.ui = ui;
+    this.units = new Units(ctx, unitsTotal, sides);
   }
 
   startSimulation() {
     this.ui.clearFrame();
 
-    this.units.forEach((unit) => {
-      unit.move();
-    });
-  
+    this.units.moveUnits();
+
     this.ui.drawInterface();
-  
-    window.requestAnimationFrame(this.startSimulation.bind(this));
+
+    // window.requestAnimationFrame(this.startSimulation.bind(this));
   }
 }
 
@@ -110,17 +171,21 @@ class Game {
 
 function getDirectionBySide(side) {
   switch (side) {
-    case PLAYER1_COLOR: return 1
-    case PLAYER2_COLOR: return -1
+    case PLAYER1_COLOR:
+      return 1;
+    case PLAYER2_COLOR:
+      return -1;
   }
 }
 
 // main
 
-(function() {
+(function () {
   const canv = document.getElementById('canv');
   const $ctx = canv.getContext('2d');
 
-  new Game($ctx, new UI($ctx), 50, [PLAYER1_COLOR, PLAYER2_COLOR])
-    .startSimulation()
-}());
+  new Game($ctx, new UI($ctx), 50, [
+    PLAYER1_COLOR,
+    PLAYER2_COLOR,
+  ]).startSimulation();
+})();
